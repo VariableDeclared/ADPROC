85014542957c3487a6de7a578454039339db1d91
VariableDeclared <pete@variabledeclared.com>
2015-10-28 11:44:45 +0000

    GUI Changes
    Experimenting with eventing in JTabbedPanes

--- PipesRUs/src/pipesrus/PipesRUsGUI.java
+++ PipesRUs/src/pipesrus/PipesRUsGUI.java
@@ -6,15 +6,16 @@
 package pipesrus;
 
 import java.awt.Dimension;
-import java.awt.Toolkit;
-import javax.swing.JFrame;
-import javax.swing.JTabbedPane;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
 
 /**
  *
  * @author pete
  */
-public class PipesRUsGUI extends JFrame {
+public class PipesRUsGUI extends JFrame implements ActionListener {
     
     private Dimension userWindow;
     public PipesRUsGUI(double sizePercentage)
@@ -29,14 +30,44 @@ public class PipesRUsGUI extends JFrame {
         
         initInterface();
     }
-    
+    private void print(String value)
+    {
+        System.out.println(value);
+    }
+    /**
+     * 
+     * @param evnt 
+     */
+    @Override
+    public synchronized void actionPerformed(ActionEvent evnt)
+    {
+        //Not sure yet......
+        print("EVNETFAFGFWKRFW");
+    }
+
     private void initInterface()
     {
         //this.setLayout(new FlowLayout());
         JTabbedPane mainInterface = new JTabbedPane();
+        //this.setLayout(new FlowLayout());
+        JPanel tabOnePanel = new JPanel();
+        JPanel tabTwoPanel = new JPanel();
+        //init panels
+        JButton goButton = new JButton("Go");
+        goButton.addActionListener(this);
+        tabOnePanel.add(goButton);
         
-        mainInterface.addTab("Tab 1", null);
-        mainInterface.addTab("Tab 2", null);
+        
+        mainInterface.addTab("Tab 1", tabOnePanel);
+        mainInterface.addTab("Tab 2", tabTwoPanel);
+         
+        mainInterface.addChangeListener(new ChangeListener () {
+            public void stateChanged(ChangeEvent ent)
+            {
+                print("SOMETHING HAPPENED");
+            }
+            
+        });
         //mainInterface.setLayout(new FlowLayout());
         this.add(mainInterface);
         
