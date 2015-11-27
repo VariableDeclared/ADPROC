1b13a5b0ec4982a047b5dc14f01039cc66e1993e
VariableDeclared <pete@variabledeclared.com>
2015-11-27 15:25:52 +0000

    Initial commit :+1:

---
+++ PipesRUs/src/pipesrus/sql/SQLManager.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pipesrus.sql;
+import java.sql.*;
+/**
+ *
+ * @author 732011 <up732011@myport.ac.uk>
+ */
+public class SQLManager {
+    
+}
