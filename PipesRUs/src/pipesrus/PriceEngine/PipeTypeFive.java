3b02db73dfa8dae9f5efb6f119305d161aa4b417
VariableDeclared <pete@variabledeclared.com>
2015-12-01 01:36:17 +0000

    Merge changes from dev into SQLLite

--- PipesRUs/src/pipesrus/PriceEngine/PipeTypeFive.java
+++ PipesRUs/src/pipesrus/PriceEngine/PipeTypeFive.java
@@ -46,7 +46,7 @@ public class PipeTypeFive extends PipeTypeFour implements IOuterReinforcedPipe
         double price = super.getPrice();
         
         //outer reinforcement
-        price += this._priceForPlastic * 1.15;
+        price += this._priceForPlastic * 0.15;
         
         return price;
         
